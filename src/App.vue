fb5493e237afad43fd4618a97d4072cfdef37256
malinatrash <malinatrashh@gmail.com>
2023-01-28 19:25:15 +0800

    Научился работать со стилями. Поработал с примитивными слушателями событий (@click). Пока что Vue очень нравится.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
-<script setup>
-</script>
-
 <template>
-  <h1>Vue fundamental course</h1>
+  <div class="post">
+    <div><strong>Title: </strong>Post about Vue</div>
+    <div><strong>Description: </strong>Vue is progressive framework</div>
+  </div>
+  <div class="post">
+    <div><strong>Title: </strong>Post about Vue</div>
+    <div><strong>Description: </strong>Vue is progressive framework</div>
+  </div>
+  <div class="post">
+    <div><strong>Title: </strong>Post about Vue</div>
+    <div><strong>Description: </strong>Vue is progressive framework</div>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  margin: 5px;
+  padding: 15px;
+  border: 1px solid lawngreen;
+  background: lawngreen;
+  border-radius: 25px;
 }
 </style>
