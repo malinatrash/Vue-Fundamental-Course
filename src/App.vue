cfac351c2e515126d4d6ad3af9a41f557dd8b9c2
malinatrash <malinatrashh@gmail.com>
2023-01-28 19:07:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Vue fundamental course</h1>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+</style>
