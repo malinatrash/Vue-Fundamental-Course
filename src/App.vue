7caba5ebaec0bc76aa5c367602515bb84b7f244a
malinatrash <malinatrashh@gmail.com>
2023-01-30 01:51:13 +0800

    Посмотрел, как работает v-for

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="post">
-    <div><strong>Title: </strong>Post about Vue</div>
-    <div><strong>Description: </strong>Vue is progressive framework</div>
-  </div>
-  <div class="post">
-    <div><strong>Title: </strong>Post about Vue</div>
-    <div><strong>Description: </strong>Vue is progressive framework</div>
-  </div>
-  <div class="post">
-    <div><strong>Title: </strong>Post about Vue</div>
-    <div><strong>Description: </strong>Vue is progressive framework</div>
+  <div>
+    <form>
+      <h2>Add Post</h2>
+      <input class="input" type="text" placeholder="Title">
+      <input class="input" type="text" placeholder="Description">
+      <button>Add</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Title: </strong>{{post.title}}</div>
+      <div><strong>Description: </strong>{{post.body}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JS', body: 'DescriptionJS'},
+        {id: 1, title: 'Python', body: 'DescriptionPython'},
+        {id: 1, title: 'LALLA', body: 'description LALALla'},
+      ]
+    }
+  }
 }
 </script>
 
@@ -27,11 +34,23 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  margin: 20px;
+}
+
 .post {
   margin: 5px;
   padding: 15px;
-  border: 1px solid lawngreen;
+  border: 3px solid green;
   background: lawngreen;
   border-radius: 25px;
+}
+
+.input {
+  width: 300px;
+  border: 2px solid green;
+  padding: 10px 15px;
+  margin-top: 15px;
+  border-radius:25px;
 }
 </style>
