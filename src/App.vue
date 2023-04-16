50b6abcb32486e68e9a9844ffdaa7acc58f82d37
Pavel <malinatrashh@gmail.com>
2023-04-16 19:22:10 +0800

    Поработал с пропсами

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,60 @@
-<template>
-  <div>
-    <form>
-      <h2>Add Post</h2>
-      <input class="input" type="text" placeholder="Title">
-      <input class="input" type="text" placeholder="Description">
-      <button>Add</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Title: </strong>{{post.title}}</div>
-      <div><strong>Description: </strong>{{post.body}}</div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      posts: [
-        {id: 1, title: 'JS', body: 'DescriptionJS'},
-        {id: 1, title: 'Python', body: 'DescriptionPython'},
-        {id: 1, title: 'LALLA', body: 'description LALALla'},
-      ]
-    }
+<script setup>
+import { ref, computed } from 'vue'
+import OperationButton from "./Components/Operationbutton.vue";
+const OPERATIONS = ['+', '-', '*', '/']
+let a = ref(11)
+let b = ref(20)
+let c = computed(() => {
+  switch (operation.value) {
+    case '+':
+      return a.value + b.value
+    case '-':
+      return a.value - b.value
+    case '/':
+      return a.value / b.value
+    case '*':
+      return a.value * b.value
   }
+})
+let operation = ref('+')
+const setOp = value => {
+  operation.value = value
 }
 </script>
 
+<template>
+  <div class="container">
+    <input
+        type="number"
+        class="form-control frm"
+        v-model="a"
+    />
+    <input
+        type="number"
+        class="form-control frm"
+        v-model="b"
+    />
+    <div class="row mt-2">
+      <div
+          class="col-3"
+          v-for="op in OPERATIONS"
+      >
+        <OperationButton
+            @clicked="setOp(op)"
+            :operation="op"
+        />
+      </div>
+    </div>
+    <hr />
+    <h1>{{ a }} {{ operation }} {{ b }} = {{ c }}</h1>
+  </div>
+</template>
+
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.app {
-  margin: 20px;
-}
-
-.post {
-  margin: 5px;
-  padding: 15px;
-  border: 3px solid green;
-  background: lawngreen;
-  border-radius: 25px;
-}
-
-.input {
-  width: 300px;
-  border: 2px solid green;
-  padding: 10px 15px;
+.container {
   margin-top: 15px;
-  border-radius:25px;
+}
+.frm {
+  margin-top: 15px;
 }
 </style>
---
+++ src/Components/Operationbutton.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, computed } from 'vue'
+const props = defineProps({
+  operation: {
+    type: String,
+    default: '+',
+  },
+  activeOperation: {
+    type: String
+  }
+})
+const emit = defineEmits(['clicked'])
+const cls = computed(() => {
+  return {
+    'btn-primary' : props.operation === props.activeOperation,
+    'btn-outline-primary' : props.operation !== props.activeOperation
+  }
+})
+</script>
+
+<template>
+  <button
+      :class="cls"
+      class="btn w-100"
+      @click="emit('clicked')"
+      value="c.value"
+  >
+    {{ operation }}
+  </button>
+</template>
+
+<style></style>
