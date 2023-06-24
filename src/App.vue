3291ec0a68a6ff26d1a789cf10b8262c1b4acbf7
Pavel <malinatrashh@gmail.com>
2023-06-24 23:03:51 +0800

    Перенес на электрон и прикрутил tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
-<script setup>
-import CalculatorPage from "./Pages/CalculatorPage.vue";
-import {computed, onBeforeMount, ref} from "vue";
-import Student from "./Components/Student.vue";
-const students = ref([])
-const filterText = ref("")
-onBeforeMount(async () => {
-  let r = await fetch("students2022.json")
-  let data = await r.json()
-  students.value = data
-})
-
-const filteredStudents = computed(() => {
-  return students.value.filter((s) => {
-    return s.first_name.trim().toLowerCase().includes(filterText.value)
-        || s.last_name.trim().toLowerCase().includes(filterText.value)
-  })
-})
-</script>
+<script setup></script>
 
 <template>
-  <div class="row m-2">
-    <div class="col-12">
-      <input placeholder="Поиск" type="text" class="form-control" v-model="filterText">
-    </div>
-  </div>
-  <div v-for="student in filteredStudents">
-    <Student :student="student"/>
-  </div>
+	<div class="flex justify-center items-center w-screen h-screen">
+		<div class="flex-col">
+			<div class="bg-slate-200 rounded-lg w-96 h-96 shadow-md"></div>
+			<h3 class="mt-3 text-center text-4xl">Последнее лето</h3>
+			<h5 class="mt-1 text-center text-xl">ssshhhiiittt!</h5>
+		</div>
+	</div>
 </template>
-
-<style></style>
