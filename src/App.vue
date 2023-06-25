7947ceb93e48e24da446567f53bd993c98c0c3d6
Pavel <malinatrashh@gmail.com>
2023-06-25 23:48:14 +0800

    чето наделал

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+</script>
 
 <template>
 	<div class="flex justify-center items-center w-screen h-screen">
-		<div class="flex-col">
-			<div class="bg-slate-200 rounded-lg w-96 h-96 shadow-md"></div>
-			<h3 class="mt-3 text-center text-4xl">Последнее лето</h3>
-			<h5 class="mt-1 text-center text-xl">ssshhhiiittt!</h5>
-		</div>
+		<h1>{{ count }}</h1>
 	</div>
 </template>
