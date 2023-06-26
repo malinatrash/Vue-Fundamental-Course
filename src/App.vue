30c51751153b47dbb31d095996043d3a65679989
Pavel <malinatrashh@gmail.com>
2023-06-27 02:08:27 +0800

    Разобрался зачем нужен setup побработал с пропсами

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
-<script lang="ts">
-import { ref } from 'vue'
-const count = ref(0)
-</script>
-
 <template>
-	<div class="flex justify-center items-center w-screen h-screen">
-		<h1>{{}}</h1>
+	<div v-for="track in tracks" :key="track.id">
+		<Card
+			:album="track.album"
+			:imageURL="track.image"
+			:artist="track.artist"
+			:title="track.title"
+		/>
 	</div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+
+const tracks = ref([
+	{
+		id: 1,
+		image:
+			'https://lastfm.freetls.fastly.net/i/u/300x300/b82ece1a74e59e932a1ca0ea95fd57c5.jpg',
+		title: 'Последнее лето',
+		artist: 'ssshhhiiittt!',
+		album: 'Последнее лето 2',
+	},
+	{
+		id: 2,
+		image:
+			'https://lastfm.freetls.fastly.net/i/u/300x300/b82ece1a74e59e932a1ca0ea95fd57c5.jpg',
+		title: 'Сияние',
+		artist: 'ssshhhiiittt!',
+		album: 'Последнее лето 2',
+	},
+	{
+		id: 3,
+		image:
+			'https://lastfm.freetls.fastly.net/i/u/300x300/f313530fbb54c77a3ce0bd5cf63bff0b.jpg',
+		title: 'Серф панк',
+		artist: 'ssshhhiiittt!',
+		album: 'Последнее лето 1',
+	},
+	{
+		id: 4,
+		image:
+			'https://lastfm.freetls.fastly.net/i/u/300x300/bc0555c16f5e95dee89804ab6eef9458.jpg',
+		title: 'Цветы',
+		artist: 'ssshhhiiittt!',
+		album: 'Третья жизнь',
+	},
+])
+</script>
+
+<style></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wrapper">
+		<div class="image-wrapper">
+			<img class="image" :src="imageURL" :alt="imageURL" />
+		</div>
+		<h3 class="title">{{ title }}</h3>
+		<h5 class="artist">{{ artist }}</h5>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	imageURL: String,
+	artist: String,
+	album: String,
+	title: String,
+})
+</script>
+
+<style scoped>
+.image-wrapper {
+	@apply border border-black/70 shadow-lg bg-slate-200 rounded-lg w-64 h-64 transition-all duration-200 hover:w-72 hover:h-72;
+}
+.wrapper {
+	@apply flex-col m-5;
+}
+.title {
+	@apply mt-3 text-center text-4xl;
+}
+.artist {
+	@apply mt-1 text-center text-xl;
+}
+.image-wrapper img {
+	@apply w-full h-auto;
+}
+</style>
